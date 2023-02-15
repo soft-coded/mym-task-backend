6dcd1be77c18de6d2543b0f5a2e621881c1a1e99
soft-coded <shrutanten.work@gmail.com>
2023-02-15 15:16:46 +0530

    Add to protect middleware

--- middlewares/protect.ts
+++ middlewares/protect.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 
 import { JWT_SECRET_KEY } from "../utils/constants";
 import { CustomError } from "../utils/error-handler";
+import UserModel from "../models/user";
 
-export function protect(req: Request, res: Response, next: NextFunction) {
+export async function protect(req: Request, res: Response, next: NextFunction) {
 	try {
 		if (!req.headers.authorization) throw new CustomError(401, "Unauthorized");
 
@@ -14,6 +15,9 @@ export function protect(req: Request, res: Response, next: NextFunction) {
 
 		if (!userData || !userData.email)
 			throw new CustomError(401, "Invalid authorization token");
+
+		const doesUserExist = await UserModel.exists({ email: userData.email });
+		if (!doesUserExist) throw new CustomError(403, "Forbidden");
 
 		next();
 	} catch (err) {
