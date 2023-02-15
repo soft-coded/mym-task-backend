12d50fc1e7c95823950cdfff5ee4307efd7b4391
soft-coded <shrutanten.work@gmail.com>
2023-02-15 14:16:11 +0530

    Add protect middleware

--- middlewares/protect.ts
+++ middlewares/protect.ts
@@ -1,3 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export function protect(req: Request, res: Response, next: NextFunction) {}
+import { JWT_SECRET_KEY } from "../utils/constants";
+import { CustomError } from "../utils/error-handler";
+
+export function protect(req: Request, res: Response, next: NextFunction) {
+	try {
+		if (!req.headers.authorization) throw new CustomError(401, "Unauthorized");
+
+		// Authorization = "Bearer userToken"
+		const token = req.headers.authorization.split(" ")[1];
+		const userData = jwt.verify(token, JWT_SECRET_KEY) as jwt.JwtPayload;
+
+		if (!userData || !userData.email)
+			throw new CustomError(401, "Invalid authorization token");
+
+		next();
+	} catch (err) {
+		next(err);
+	}
+}
